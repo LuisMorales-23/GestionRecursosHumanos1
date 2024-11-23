2b453230f443f359a6405989278efaf0ba1b0142
LuisMorales-23 <dev01.luis.morales@gmail.com>
2024-11-23 02:50:36 -0500

    index Departamento

--- Pages/Departamento/Index.cshtml.cs
+++ Pages/Departamento/Index.cshtml.cs
@@ -4,11 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace GestionRecursosHumanos1.Pages.Departamentos
-
+namespace GestionRecursosHumanos1.Pages.Departamento
 {
     public class IndexModel : PageModel
     {
-        
+        private readonly GestionRecursosContext _context;
+
+        public IndexModel(GestionRecursosContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Departamentos> Departamento { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Departamento != null)
+            {
+                Departamento = await _context.Departamento.ToListAsync();
+            }
+        }
     }
 }
