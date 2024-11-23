ada0165670b2e44352cabb34a2d9d12e013df8b7
LuisMorales-23 <dev01.luis.morales@gmail.com>
2024-11-23 00:47:54 -0500

    departamentos

---
+++ Pages/Departamentos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos1.Pages.Departamentos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
