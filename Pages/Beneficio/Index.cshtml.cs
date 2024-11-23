468a7fc6a194ab7351ebf1be40746986eb9ad981
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-22 23:41:11 -0500

    Index beneficios

---
+++ Pages/Beneficio/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos1.Pages.Beneficio
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
