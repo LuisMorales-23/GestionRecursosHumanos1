13bf4392f05af395933631b6992a99ff7c5464a3
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-23 00:56:00 -0500

    archivo create

---
+++ Pages/Beneficio/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos1.Pages.Beneficio
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
