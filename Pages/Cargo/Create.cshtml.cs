6ea517f2c1fe37fd6f684c048ced51874ce60bee
LuisMorales-23 <dev01.luis.morales@gmail.com>
2024-11-23 02:05:05 -0500

    create cargo

---
+++ Pages/Cargo/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using GestionRecursosHumanos.Data;
+using GestionRecursosHumanos1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos1.Pages.Cargo
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GestionRecursosContext _context;
+
+        public CreateModel(GestionRecursosContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+
+        [BindProperty]
+        public Cargos Cargo { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Cargo == null || Cargo == null)
+            {
+                return Page();
+            }
+
+            _context.Cargo.Add(Cargo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
